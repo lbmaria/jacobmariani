e8cbd752721cc2744c3adc3a0111b5891b7efe60
lbmaria <56056343+lbmaria@users.noreply.github.com>
2019-11-19 16:46:41 -0600

    Add files via upload

---
+++ js/navbar.js
@@ -0,0 +1,44 @@
+function classToggle() {
+  const navs = document.querySelectorAll('.navbar_items')
+  
+  navs.forEach(nav => nav.classList.toggle('navbar_toggleShow'));
+}
+document.querySelector('.navbar_link-toggle')
+  .addEventListener('click', classToggle);
+
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
